ed62af290b5986f977d59daab0c738ee41b6ddf4
djack <djack13rocha@gmail.com>
2022-01-03 16:24:54 -0300

    add a more optimized way of doing the problem 1070

--- Iniciante/1070.py
+++ Iniciante/1070.py
@@ -14,3 +14,17 @@ for i in  itertools.count(start=num):
 for impar in impares:
     print('{}'.format(impar))
 
+# Other solution
+
+#x = int(input())
+
+##Calculo do menor ímpar maior ou igual à x
+##  x%2 retorna 1 se x é ímpar
+##  (x+1)%2 retorna 1 se x é par
+##  se somamos 1 a um x par, ele se torna ímpar
+#x += (x+1)%2
+
+# Loop para imprimir os seis valores
+#for i in range(6):
+#    print(x)
+#    x += 2
