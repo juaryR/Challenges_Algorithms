1f7f5a91d9fa2a6ba0ed2cb8081e3a6fa3df9cd2
djack <djack13rocha@gmail.com>
2022-04-30 18:50:23 -0100

    add fast solution to groupAnagram

--- LeetCode/groupAnagrams.py
+++ LeetCode/groupAnagrams.py
@@ -1,18 +1,29 @@
 # my solutions 
+# class Solution:
+#     def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
+#         hashMap = {}
+#         for key,string in enumerate(strs):
+#             string_sort = "".join(sorted(string))
+#             if not string_sort in hashMap:
+#                 hashMap[string_sort] = [key]
+#             else:
+#                 hashMap[string_sort].append(key)
+#         result = []
+#         temp_result = []
+#         for valeu in list(hashMap.values()):
+#             for key in valeu:
+#                 temp_result.append(strs[key])
+#             result.append(temp_result)
+#             temp_result =[]
+#         return result 
+# Fast solution 
 class Solution:
-    def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
+    def groupAnagrams(self, strs: list[str]) -> list[List[str]]:
         hashMap = {}
         for key,string in enumerate(strs):
             string_sort = "".join(sorted(string))
             if not string_sort in hashMap:
-                hashMap[string_sort] = [key]
+                hashMap[string_sort] = [strs[key]]
             else:
-                hashMap[string_sort].append(key)
-        result = []
-        temp_result = []
-        for valeu in list(hashMap.values()):
-            for key in valeu:
-                temp_result.append(strs[key])
-            result.append(temp_result)
-            temp_result =[]
-        return result 
+                hashMap[string_sort].append(strs[key])
+        return hashMap.values()
