53d0ce4521b5ed3210a47169c8b2c797508b0def
djack <djack13rocha@gmail.com>
2022-05-29 07:55:45 -0300

    add problem 33 from leetcode

---
+++ LeetCode/searchInRotatedSortedArray.py
@@ -0,0 +1,23 @@
+## https://www.youtube.com/watch?v=U8XENwh8Oy8
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l,r = 0, len(nums)-1
+        
+        while l <= r:
+            mid = (l+r)//2
+            
+            if nums[mid] == target:
+                return mid
+            # left 
+            if nums[l] <= nums[mid]:
+                if target > nums[mid] or target < nums[l]:
+                    l = mid+1
+                else:
+                    r =mid-1
+            # right
+            else:
+                if target < nums[mid] or target > nums[r]:
+                    r = mid -1
+                else:
+                    l = mid+1
+        return -1
